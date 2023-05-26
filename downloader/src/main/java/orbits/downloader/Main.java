16c59ed52e32ab348e34e3cc059e360375565df5
DasBabyPixel <43410952+DasBabyPixel@users.noreply.github.com>
2023-05-26 14:22:32 +0200

    downloader

--- downloader/src/main/java/orbits/downloader/Main.java
+++ downloader/src/main/java/orbits/downloader/Main.java
@@ -1,10 +1,37 @@
 package orbits.downloader;
 
+import org.kohsuke.github.GHAsset;
+import org.kohsuke.github.GHRelease;
+import org.kohsuke.github.GHRepository;
+import org.kohsuke.github.GitHub;
+
+import javax.sound.midi.Soundbank;
+import java.io.IOException;
 import java.io.InputStream;
+import java.net.Proxy;
+import java.net.ProxySelector;
 import java.net.URL;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.StandardCopyOption;
+import java.util.concurrent.ExecutionException;
 
 public class Main {
-    public static void main(String[] args) {
-        InputStream in = new URL("")
+    public static void main(String[] args) throws IOException, ExecutionException, InterruptedException {
+        System.setProperty("java.net.useSystemProxies", "true");
+        URL url = new URL("https://github.com/DasBabyPixel/Orbits/releases/download/1.0/orbits.bat");
+        InputStream in = url.openStream();
+
+        Path temp = Files.createTempFile("orbits", ".bat");
+        Files.copy(in, temp, StandardCopyOption.REPLACE_EXISTING);
+        in.close();
+
+        Process process = Runtime.getRuntime().exec("\"" + temp.toAbsolutePath() + "\"");
+        InputStream is = process.getInputStream();
+        int b;
+        while ((b = is.read()) != -1) {
+            System.out.write(b);
+        }
+        process.onExit().get();
     }
 }
