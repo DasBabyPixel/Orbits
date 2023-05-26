36f04e38551d200294bfa3f240511729902894ec
DasBabyPixel <43410952+DasBabyPixel@users.noreply.github.com>
2023-05-26 08:54:39 +0200

    downloader

---
+++ downloader/src/main/java/orbits/downloader/Main.java
@@ -0,0 +1,10 @@
+package orbits.downloader;
+
+import java.io.InputStream;
+import java.net.URL;
+
+public class Main {
+    public static void main(String[] args) {
+        InputStream in = new URL("")
+    }
+}
